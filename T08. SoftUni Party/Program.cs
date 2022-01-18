bef6b74b375664085fa4ce9b23cfd579f013fd81
Storm603 <93555141+Storm603@users.noreply.github.com>
2022-01-19 00:11:25 +0200

    Add files via upload

--- T08. SoftUni Party/Program.cs
+++ T08. SoftUni Party/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace T08._SoftUni_Party
 {
@@ -6,7 +7,50 @@ namespace T08._SoftUni_Party
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            HashSet<string> VIP = new HashSet<string>();
+            HashSet<string> regular = new HashSet<string>();
+
+            string input = Console.ReadLine();
+            while (input != "PARTY")
+            {
+                if (input[0] > 47 && input[0] < 58)
+                {
+                    VIP.Add(input);
+                }
+                else
+                {
+                    regular.Add(input);
+                }
+
+                input = Console.ReadLine();
+            }
+
+            input = Console.ReadLine();
+            while (input != "END")
+            {
+                if (VIP.Contains(input))
+                {
+                    VIP.Remove(input);
+                }
+                else if (regular.Contains(input))
+                {
+                    regular.Remove(input);
+                }
+
+                input = Console.ReadLine();
+            }
+
+            Console.WriteLine(VIP.Count + regular.Count);
+
+            foreach (string guest in VIP)
+            {
+                Console.WriteLine(guest);
+            }
+
+            foreach (string guest in regular)
+            {
+                Console.WriteLine(guest);
+            }
         }
     }
 }
