4717090f37e6a06d01c769f06f2e47b25030a1f1
Storm603 <93555141+Storm603@users.noreply.github.com>
2022-01-18 23:47:29 +0200

    Add files via upload

---
+++ T06. Record Unique Names/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace T06._Record_Unique_Names
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            HashSet<string> collection = new HashSet<string>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string name = Console.ReadLine();
+
+                collection.Add(name);
+            }
+
+            foreach (string name in collection)
+            {
+                Console.WriteLine(name);
+            }
+        }
+    }
+}
---
+++ T07. Parking Lot/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace T07._Parking_Lot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<string> plateCollection = new HashSet<string>();
+
+            string input = Console.ReadLine();
+
+            while (input != "END")
+            {
+                string[] split = input.Split(", ");
+
+                string action = split[0];
+                string plate = split[1];
+
+                if (action == "IN")
+                {
+                    plateCollection.Add(plate);
+                }
+                else if (action == "OUT")
+                {
+                    //if (plateCollection.Count == 0)
+                    //{
+                    //    continue;
+                    //}
+
+                    plateCollection.Remove(plate);
+                }
+
+                input = Console.ReadLine();
+            }
+
+            if (plateCollection.Count == 0)
+            {
+                Console.WriteLine("Parking Lot is Empty");
+                return;
+            }
+
+            foreach (string plate in plateCollection)
+            {
+                Console.WriteLine(plate);
+            }
+
+        }
+    }
+}
